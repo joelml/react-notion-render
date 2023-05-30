0ad491e944a63320132f54d5aee4c7206a2dffbe
Joel <joel.lundgren@gmail.com>
2023-05-30 15:55:09 +0200

    Changed the url for the links

--- src/components/common/Link/index.tsx
+++ src/components/common/Link/index.tsx
@@ -8,12 +8,12 @@ export interface Props {
 
 function Link({ url, children, className }: Props) {
   const redirectProps = url.startsWith('#') || url.startsWith('/') || url.startsWith('https://sebx.io')
-    ? {}
+    ? {target: '_self'}
     : {
         target: '_blank', rel: 'noreferrer'
       }
   return (
-    <a href={url} className={className} {...redirectProps}>
+    <a href={url.startsWith('https://sebx.io') ? url.split("https://sebx.io")[1] : url} className={className} {...redirectProps}>
       {children}
     </a>
   )
